d670f4f34834b4f37b21eb6733f582c690f4f272
樱凌准 <SakuraSinojun@gmail.com>
2017-09-10 11:59:03 +0800

    update color when press.
    Change-Id: Ic334ced96c1e72ac7d2e895c1553c0f0705b19cc

---
+++ index/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,minimum-scale=0.1,maximum-scale=10,initial-scale=1.0,user-scalable=yes"/>
+
+        <link rel="stylesheet" href="[:vendor]/bootstrap/css/bootstrap.css" type="text/css">
+        <link rel="stylesheet" href="[:vendor]/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="[:vendor]/sb-admin/sb-admin-2.min.css">
+       
+        <script src="[:vendor]/jquery/jquery-2.1.1.min.js"></script>
+        <script src="[:vendor]/bootstrap/js/bootstrap.min.js"></script>
+        <script src="[:vendor]/vue/vue.js"></script>
+        <script src="[:vendor]/metisMenu/metisMenu.min.js"></script>
+        <script src="[:vendor]/sb-admin/sb-admin-2.min.js"></script>
+        <title></title>
+        <script>
+            $(document).ready(function() {
+                var color=getRandomColor();
+                $("body").css("background-color", color); 
+                $("body").keydown(function() {
+                    var color=getRandomColor();
+                    $("body").css("background-color", color); 
+                });
+            });
+            var getRandomColor = function(){    
+                return  '#' +    
+                    (function(color){    
+                        return (color +=  '0123456789abcdef'[Math.floor(Math.random()*16)])    
+                            && (color.length == 6) ?  color : arguments.callee(color);    
+                    })('');    
+            } 
+        </script>
+        <style>
+            body {width: 100%; height:500px;}
+        </style>
+    </head>
+    <body>
+    </body>
+</html>
+
+
+
